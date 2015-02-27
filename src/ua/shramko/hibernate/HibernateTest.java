8254f6acf1a1daa030b91c78ee3c58d464eb7942
shramko82 <shramko82@gmail.com>
2015-02-27 06:08:50 +0200

    Hibernate
    Starting with Hibernate

---
+++ src/ua/shramko/hibernate/HibernateTest.java
@@ -0,0 +1,33 @@
+package ua.shramko.hibernate;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateTest {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		
+		Users user = new Users();
+		user.setName("Alex");
+		user.setLastName("Shramko");
+		user.setDate_Birth(new Date(82,3,25));
+		
+		Configuration config = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+		           config.getProperties()).build();
+		SessionFactory sessionFactory = config.buildSessionFactory(serviceRegistry);
+
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		
+	}
+
+}
---
+++ src/ua/shramko/hibernate/Users.java
@@ -0,0 +1,43 @@
+package ua.shramko.hibernate;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Users {
+
+	@Id
+	private int id;
+	private String Name;
+	private String LastName;
+	private Date Date_Birth;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public Date getDate_Birth() {
+		return Date_Birth;
+	}
+	public void setDate_Birth(Date date_Birth) {
+		Date_Birth = date_Birth;
+	}
+	
+	
+}
